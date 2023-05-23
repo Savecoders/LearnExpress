09dc36ce692b9915aad35cb2f6c783f65d35d725
Savecoders <pablopincay2003@gmail.com>
2023-05-22 21:13:10 -0500

    parameters in routes and query

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,12 +15,50 @@ router.get("/products", (req, res) => {
     {
       id: 1,
       name: "Product 1",
+      price: 100,
     },
     {
       id: 2,
       name: "Product 2",
+      price: 200,
     },
   ]);
+});
+
+// get products by id
+
+router.get("/products/:id", (req, res) => {
+  /* `req` is an object that represents the HTTP request made by the client
+  to the server. It contains information about the request such as the
+  URL, HTTP method, headers, and any data sent in the request body. The
+  server uses this information to process the request and generate an
+  appropriate response. */
+  const { id } = req.params;
+  res.json({
+    id,
+    name: `Product ${id}`,
+    price: 100,
+  });
+});
+
+// two parameters in the route
+
+router.get("/categories/:categoryId/products/:productId", (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+// query parameters
+// http://localhost:3000/users?name=John&age=30
+router.get("/users", (req, res) => {
+  const { name, age } = req.query;
+  res.json({
+    name,
+    age,
+  });
 });
 
 // method res is used to send a response to the client
